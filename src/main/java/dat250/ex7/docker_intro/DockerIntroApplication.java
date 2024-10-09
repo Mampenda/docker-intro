eb121a68465a6acce3b89274f8968e6671a9276b
Mampenda <amalie.mampenda@gmail.com>
2024-10-09 12:50:06 +0200

    Initialized git

---
+++ src/main/java/dat250/ex7/docker_intro/DockerIntroApplication.java
@@ -0,0 +1,13 @@
+package dat250.ex7.docker_intro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerIntroApplication.class, args);
+	}
+
+}
